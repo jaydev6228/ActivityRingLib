31ae1ef27f2a71af8451ae738cfd8771e70d3c3f
Jaydev Baloliya <53218724+jaydev6228@users.noreply.github.com>
2022-05-09 16:55:47 +0900

    initial commit

---
+++ ActivityRing/ActivityRing.h
@@ -0,0 +1,18 @@
+//
+//  ActivityRing.h
+//  ActivityRing
+//
+//  Created by Vebuin_Jaydev on 2022/05/09.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ActivityRing.
+FOUNDATION_EXPORT double ActivityRingVersionNumber;
+
+//! Project version string for ActivityRing.
+FOUNDATION_EXPORT const unsigned char ActivityRingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ActivityRing/PublicHeader.h>
+
+
